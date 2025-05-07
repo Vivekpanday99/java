429ede3cbb5a64f771ff297988df8e9f8f5c00fe
vivekpanday18java <vivekpandey7299@gmail.com>
2025-05-07 11:21:02 +0530

    j

--- stringshort/hello.java/Main.java
+++ stringshort/hello.java/Main.java
@@ -40,53 +40,76 @@
 // //     }
 // // }
 // //     class Main{
-// //     public static void main(String[] args) {
-// //         Vehicle obj=new Bike();
-// //         obj.run();
-// //     }
-// // }   
+// // //     public static void main(String[] args) {
+// // //         Vehicle obj=new Bike();
+// // //         obj.run();
+// // //     }
+// // // }   
 
-// class parent{
-//     void sum(int a){
-//         System.out.println(a);
+// // class parent{
+// //     void sum(int a){
+// //         System.out.println(a);
+// //     }
+// //     void sum(int a,int b){
+// //         System.out.println(a+b);
+// //     }
+// // }
+// // class Hello{
+// //     public static void main(String[] args) {
+// //         parent obj=new parent();
+// //         obj.sum(87);
+// //         obj.sum(87,98);
+// //     }
+
+// // }
+
+// abstract  class payment{
+//     abstract void pay(int a);
+//     void succes(){
+//         System.out.println("payment done");
 //     }
-//     void sum(int a,int b){
-//         System.out.println(a+b);
+// }
+// class upipayment extends payment{
+//     void pay(int a){
+//         System.out.println("pay vi upi" +a);
+//     }
+// }  
+// class NetBanking extends payment{
+//     void pay(int b){
+//         System.out.println("pay via netbanking" +b);
+
 //     }
 // }
 // class Hello{
 //     public static void main(String[] args) {
-//         parent obj=new parent();
-//         obj.sum(87);
-//         obj.sum(87,98);
+//         payment obj=new upipayment();
+//         obj.pay(21000);
+//         obj.succes();
+//         payment obj1=new NetBanking();
+//         obj1.pay(51000);
+//         obj1.succes();
 //     }
-
 // }
 
-abstract  class payment{
-    abstract void pay(int a);
-    void succes(){
-        System.out.println("payment done");
+
+abstract class Shape{
+    abstract void area(int a);
+}
+class Circle extends Shape{
+    void area(int a){
+        System.out.println("area of circle is "+(3.14*a*a));
     }
 }
-class upipayment extends payment{
-    void pay(int a){
-        System.out.println("pay vi upi" +a);
-    }
-}  
-class NetBanking extends payment{
-    void pay(int b){
-        System.out.println("pay via netbanking" +b);
-
+class Rectangle extends Shape{
+    void area(int a){
+        System.out.println("area of rectangle is "+(a*a));
     }
 }
 class Hello{
-    public static void main(String[] args) {
-        payment obj=new upipayment();
-        obj.pay(21000);
-        obj.succes();
-        payment obj1=new NetBanking();
-        obj1.pay(51000);
-        obj1.succes();
+    public static void main(String[] args){
+        Shape obj=new Circle();
+        obj.area(10);
+        Shape obj1=new Rectangle();
+        obj1.area(10);
     }
 }
